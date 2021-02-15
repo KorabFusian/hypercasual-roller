0330805a706d0a49140a4ef1106a08e821ef631b
Jeremi <jeremifk@gmail.com>
2021-02-15 11:46:03 +0100

    Now we can move

--- Assets/RollyBall.cs
+++ Assets/RollyBall.cs
@@ -13,6 +13,22 @@ public class RollyBall : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKey("a"))
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(-4, 0, 3);
+            StartCoroutine(StopLaneChange());
+        }
+        if(Input.GetKey("d"))
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(4, 0, 3);
+            StartCoroutine(StopLaneChange());
+        }
+    }
+
+    IEnumerator StopLaneChange() 
+    {
+        yield return new WaitForSeconds(.25f);
         GetComponent<Rigidbody>().velocity = new Vector3(0,0,3);
+        Debug.Log(GetComponent<Transform>().position);
     }
 }
